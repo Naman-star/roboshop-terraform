51f69b455a3e86a4fb5b9da0bc9d95c30db74083
user <namanshetty91132@gmail.com>
2023-05-16 08:19:11 +0100

    Naman

--- servers.tf
+++ servers.tf
@@ -1,3 +1,16 @@
+data "aws_ami" "centos" {
+   owner            = [ "973714476881" ]
+  most_recent      = true
+  name_regex       = "centos-8-DevOps-practice"
+  owner            = "973714476881"
+
+}
+
+
+output "ami"{
+value = data.aws_ami.centos.image_id
+}
+
 resource "aws_instance" "frontend" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
