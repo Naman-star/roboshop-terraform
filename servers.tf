db266568d2c522620451859597f48369f4c769c9
user <namanshetty91132@gmail.com>
2023-05-16 13:53:54 +0100

    Naman

--- servers.tf
+++ servers.tf
@@ -1,3 +1,13 @@
+data "aws_ami" "centos" {
+  owners            = ["973714476881"]
+  most_recent       = true
+  name_regex        = "Centos-8-DevOps-practice"
+}
+
+
+output "ami" {
+value = data.aws_ami.centos.image_id
+}
 
 variable "instance_type" {
  default = "t3.micro"
