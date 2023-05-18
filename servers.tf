d2a7161824449300be4a8152b6703e9f267bc90f
user <namanshetty91132@gmail.com>
2023-05-18 14:26:10 +0100

    Naman

--- servers.tf
+++ servers.tf
@@ -17,181 +17,186 @@ variable "instance_type" {
  default = "t3.small"
 }
 
+variable "components" {
+  default = [ "frontend", "mongodb", "catalogue"]
+}
+
 resource "aws_instance" "frontend" {
+  count         = length(var.components)
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = var.instance_type
   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
   tags = {
-    Name = "frontend"
+    Name = var.components[count.index]
   }
-}
 
 
-resource "aws_route53_record" "frontend" {
-  zone_id = "Z08621443HT6YNQD1Z6GT"
-  name    = "frontend-dev.ndevopsb72.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.frontend.private_ip]
-}
+
+//resource "aws_route53_record" "frontend" {
+//  zone_id = "Z08621443HT6YNQD1Z6GT"
+//  name    = "frontend-dev.ndevopsb72.online"
+//  type    = "A"
+//  ttl     = 30
+//  records = [aws_instance.frontend.private_ip]
+//
 
 
-output "frontend" {
-   value=aws_instance.frontend.public_ip
-}
+//output "frontend" {
+//   value=aws_instance.frontend.public_ip
+//
 
-resource "aws_instance" "mongodb" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+//resource "aws_instance" "mongodb" {
+//  ami           = "ami-0b5a2b5b8f2be4ec2"
+//  instance_type = var.instance_type
+//  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
-  tags = {
-    Name = "mongodb"
-  }
-}
+//  tags = {
+//    Name = "mongodb"
+//  }
+//
 
-resource "aws_route53_record" "mongodb" {
-  zone_id = "Z08621443HT6YNQD1Z6GT"
-  name    = "mongodb-dev.ndevopsb72.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mongodb.private_ip]
-}
+//resource "aws_route53_record" "mongodb" {
+//  zone_id = "Z08621443HT6YNQD1Z6GT"
+//  name    = "mongodb-dev.ndevopsb72.online"
+//  type    = "A"
+//  ttl     = 30
+//  records = [aws_instance.mongodb.private_ip]
+//
 
-resource "aws_instance" "catalogue" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+//resource "aws_instance" "catalogue" {
+//  ami           = "ami-0b5a2b5b8f2be4ec2"
+//  instance_type = var.instance_type
+//  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
-  tags = {
-    Name = "catalogue"
-  }
-}
+//  tags = {
+//    Name = "catalogue"
+//  }
+//
 
-resource "aws_route53_record" "catalogue" {
-  zone_id = "Z08621443HT6YNQD1Z6GT"
-  name    = "catalogue-dev.ndevopsb72.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.catalogue.private_ip]
-}
+//resource "aws_route53_record" "catalogue" {
+//  zone_id = "Z08621443HT6YNQD1Z6GT"
+//  name    = "catalogue-dev.ndevopsb72.online"
+//  type    = "A"
+//  ttl     = 30
+//  records = [aws_instance.catalogue.private_ip]
+//
 
-resource "aws_instance" "redis" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+//resource "aws_instance" "redis" {
+//  ami           = "ami-0b5a2b5b8f2be4ec2"
+//  instance_type = var.instance_type
+//  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
-  tags = {
-    Name = "redis"
-  }
-}
+//  tags = {
+//    Name = "redis"
+//  }
+//
 
-resource "aws_route53_record" "redis" {
-  zone_id = "Z08621443HT6YNQD1Z6GT"
-  name    = "redis-dev.ndevopsb72.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.redis.private_ip]
-}
+//resource "aws_route53_record" "redis" {
+//  zone_id = "Z08621443HT6YNQD1Z6GT"
+//  name    = "redis-dev.ndevopsb72.online"
+//  type    = "A"
+//  ttl     = 30
+//  records = [aws_instance.redis.private_ip]
+//
 
-resource "aws_instance" "user" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+//resource "aws_instance" "user" {
+//  ami           = "ami-0b5a2b5b8f2be4ec2"
+//  instance_type = var.instance_type
+//  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
-  tags = {
-    Name = "user"
-  }
-}
+//  tags = {
+//    Name = "user"
+//  }
+//
 
-resource "aws_route53_record" "user" {
-  zone_id = "Z08621443HT6YNQD1Z6GT"
-  name    = "user-dev.ndevopsb72.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.user.private_ip]
-}
+//resource "aws_route53_record" "user" {
+//  zone_id = "Z08621443HT6YNQD1Z6GT"
+//  name    = "user-dev.ndevopsb72.online"
+//  type    = "A"
+//  ttl     = 30
+//  records = [aws_instance.user.private_ip]
+//
 
-resource "aws_instance" "cart" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+//resource "aws_instance" "cart" {
+//  ami           = "ami-0b5a2b5b8f2be4ec2"
+//  instance_type = var.instance_type
+//  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
-  tags = {
-    Name = "cart"
-  }
-}
+//  tags = {
+//    Name = "cart"
+//  }
+//
 
-resource "aws_route53_record" "cart" {
-  zone_id = "Z08621443HT6YNQD1Z6GT"
-  name    = "cart-dev.ndevopsb72.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.cart.private_ip]
-}
+//resource "aws_route53_record" "cart" {
+//  zone_id = "Z08621443HT6YNQD1Z6GT"
+//  name    = "cart-dev.ndevopsb72.online"
+//  type    = "A"
+//  ttl     = 30
+//  records = [aws_instance.cart.private_ip]
+//
 
-resource "aws_instance" "mysql" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+//resource "aws_instance" "mysql" {
+//  ami           = "ami-0b5a2b5b8f2be4ec2"
+//  instance_type = var.instance_type
+//  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
-  tags = {
-    Name = "mysql"
-  }
-}
+//  tags = {
+//    Name = "mysql"
+//  }
+//
 
-resource "aws_route53_record" "mysql" {
-  zone_id = "Z08621443HT6YNQD1Z6GT"
-  name    = "mysql-dev.ndevopsb72.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mysql.private_ip]
-}
+//resource "aws_route53_record" "mysql" {
+//  zone_id = "Z08621443HT6YNQD1Z6GT"
+//  name    = "mysql-dev.ndevopsb72.online"
+//  type    = "A"
+//  ttl     = 30
+//  records = [aws_instance.mysql.private_ip]
+//
 
-resource "aws_instance" "shipping" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+//resource "aws_instance" "shipping" {
+//  ami           = "ami-0b5a2b5b8f2be4ec2"
+//  instance_type = var.instance_type
+//  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
-  tags = {
-    Name = "shipping"
-  }
-}
+//  tags = {
+//    Name = "shipping"
+//  }
+//
 
-resource "aws_instance" "rabbitmq" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+//resource "aws_instance" "rabbitmq" {
+//  ami           = "ami-0b5a2b5b8f2be4ec2"
+//  instance_type = var.instance_type
+//  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
-  tags = {
-    Name = "rabbitmq"
-  }
-}
+//  tags = {
+//    Name = "rabbitmq"
+//  }
+//
 
-resource "aws_route53_record" "rabbitmq" {
-  zone_id = "Z08621443HT6YNQD1Z6GT"
-  name    = "rabbitmq-dev.ndevopsb72.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.rabbitmq.private_ip]
-}
+//resource "aws_route53_record" "rabbitmq" {
+//  zone_id = "Z08621443HT6YNQD1Z6GT"
+//  name    = "rabbitmq-dev.ndevopsb72.online"
+//  type    = "A"
+//  ttl     = 30
+//  records = [aws_instance.rabbitmq.private_ip]
+//
 
-resource "aws_instance" "payment" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+//resource "aws_instance" "payment" {
+//  ami           = "ami-0b5a2b5b8f2be4ec2"
+//  instance_type = var.instance_type
+//  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
-  tags = {
-    Name = "payment"
-  }
-}
+//  tags = {
+//    Name = "payment"
+//  }
+//
 
-resource "aws_route53_record" "payment" {
-  zone_id = "Z08621443HT6YNQD1Z6GT"
-  name    = "payment-dev.ndevopsb72.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.payment.private_ip]
-}
+//resource "aws_route53_record" "payment" {
+//  zone_id = "Z08621443HT6YNQD1Z6GT"
+//  name    = "payment-dev.ndevopsb72.online"
+//  type    = "A"
+//  ttl     = 30
+//  records = [aws_instance.payment.private_ip]
+//
 
