69d30879c773df5b95aa2dfa6be0cfe4516f4538
user <namanshetty91132@gmail.com>
2023-05-19 17:38:42 +0100

    Naman

--- servers.tf
+++ servers.tf
@@ -67,9 +67,9 @@ variable "components" {
 }
 
 resource "aws_instance" "instance" {
- for_each         = var.components
- ami           = data.aws_ami.centos.image_id
- instance_type = each.value["instance_type"]
+ for_each               = var.components
+ ami                    = data.aws_ami.centos.image_id
+ instance_type          = each.value["instance_type"]
  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
  tags = {
      Name = each.value["name"]
