0e743d088431b7fba4cf76781ce0ea3e721ed180
user <namanshetty91132@gmail.com>
2023-05-19 17:26:43 +0100

    Naman

--- servers.tf
+++ servers.tf
@@ -75,7 +75,7 @@ resource "aws_instance" "instance" {
      Name = each.value["name"]
    }
  }
- resource "aws_route53_record" "catalogue" {
+ resource "aws_route53_record" "records" {
     for_each = var.components
    zone_id = "Z08621443HT6YNQD1Z6GT"
    name    = "${each.value["name"]}"-dev.ndevopsb72.online
