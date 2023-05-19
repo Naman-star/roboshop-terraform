41fbfeced85dfb941fabae42b625a4e385845342
user <namanshetty91132@gmail.com>
2023-05-19 17:13:09 +0100

    Naman

--- servers.tf
+++ servers.tf
@@ -18,21 +18,76 @@ variable "instance_type" {
 }
 
 variable "components" {
-  default = [ "frontend", "mongodb", "catalogue"]
+  default = {
+    frontend = {
+      name = "frontend"
+      instance_type = "t3.small"
+    }
+    mongodb = {
+          name = "mongodb"
+          instance_type = "t3.small"
+    }
+    catalogue = {
+          name = "catalogue"
+          instance_type = "t3.micro"
+    }
+    redis = {
+          name = "redis"
+          instance_type = "t3.small"
+    }
+    user = {
+          name = "user"
+          instance_type = "t3.small"
+    }
+    cart = {
+          name = "cart"
+          instance_type = "t3.micro"
+    }
+    mysql = {
+          name = "mysql"
+          instance_type = "t3.small"
+    }
+    shipping = {
+          name = "shipping"
+          instance_type = "t3.medium"
+    }
+    rabbitmq = {
+          name = "rabbitmq "
+          instance_type = "t3.small"
+    }
+    payment = {
+          name = "payment"
+          instance_type = "t3.small"
+    }
+             
+
+
+
+  }
 }
 
 resource "aws_instance" "instance" {
-  count         = length(var.components)
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+ for_each         = var.components
+ ami           = data.aws_ami.centos.image_id
+ instance_type = each.value["instance_type"]
+ vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+ tags = {
+     Name = each.value["name"]
+   }
+ }
 
 
 
-  tags = {
-    Name = var.components[count.index]
-  }
-}
+//resource "aws_instance" "instance" {
+//  count         = length(var.components)
+//  ami           = data.aws_ami.centos.image_id
+//  instance_type = var.instance_type
+//  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+
+//  tags = {
+//    Name = var.components[count.index]
+//  }
+//}
 
 //row 20-35 we applied count refer learn-terraform>03-loop>count>main.tf
 
