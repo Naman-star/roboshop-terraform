f1dbec8b9beaaae31abde8c3f782ce180a1ac074
user <namanshetty91132@gmail.com>
2023-05-23 10:16:00 +0100

    Naman

--- servers.tf
+++ servers.tf
@@ -26,7 +26,7 @@ resource "aws_instance" "instance" {
        host = self.private_ip
       }
 
-      inline [
+      inline = [
         "rm -rf roboshop-shell",
         "git clone https://github.com/Naman-star/roboshop-shell",
         "cd roboshop-shell",
