49fe4f0aae9e5c925ff7b39d2d8e7828eee03810
user <namanshetty91132@gmail.com>
2023-06-06 04:27:18 +0100

    Naman

--- module/main.tf
+++ module/main.tf
@@ -66,6 +66,7 @@ resource "aws_instance" "instance" {
    }
  }
 
+
  resource "aws_iam_role_policy" "ssm-ps-policy" {
    name = "${var.component_name}-${var.env}-ssm-ps-policy"
    role = aws_iam_role.role.id
