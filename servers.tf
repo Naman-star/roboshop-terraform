a2770f5fcc6885118c91e43ab3351998b79e3a61
user <namanshetty91132@gmail.com>
2023-06-06 03:53:28 +0100

    Naman

--- servers.tf
+++ servers.tf
@@ -1,12 +1,25 @@
-module "servers" {
- for_each       = var.components
+module "database-servers" {
+ for_each       = var.database-servers
  source         = "./module"
  component_name = each.value["name"]
  env            = var.env
  instance_type  = each.value["instance_type"]
  password       = lookup(each.value, "password", "null")
+ provioner = true
+ app_type = "db"
 }
 
+module "db-servers" {
+ depends_on = [module.database-servers]
+ for_each       = var.db-servers
+ source         = "./module"
+ component_name = each.value["name"]
+ env            = var.env
+ instance_type  = each.value["instance_type"]
+ password       = lookup(each.value, "password", "null")
+ provisioner = true
+ app_type = "app"
+}
 //output "ami" {
 //value = data.aws_ami.centos.image_id
 //}
