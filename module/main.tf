e88d65af1a522bde6caef6c7199f8e4e0f5edd3e
user <namanshetty91132@gmail.com>
2023-06-21 11:50:34 +0100

    Naman

--- module/main.tf
+++ module/main.tf
@@ -78,26 +78,27 @@ resource "aws_iam_instance_profile" "instance_profile" {
    name = "${var.component_name}-${var.env}-ssm-ps-policy"
    role = aws_iam_role.role.id
    policy = jsonencode({
-                           "Version" : "2012-10-17",
-                           "Statement" : [
-                               {
-                                   "Sid" : "VisualEditor0",
-                                   "Effect" : "Allow",
-                                   "Action" : [
-                                       "ssm:GetParameterHistory",
-                                       "ssm:GetParametersByPath",
-                                       "ssm:GetParameters",
-                                       "ssm:GetParameter"
-                                   ],
-                                   "Resource": "arn:aws:ssm:us-east-1:871868163784:parameter/${var.env}.${var.component_name}.*"
-                               },
-                               {
-                                   "Sid" : "VisualEditor1",
-                                   "Effect" : "Allow",
-                                   "Action" : "ssm:DescribeParameters",
-                                   "Resource" : "*"
-                               }
+   {
+     "Version": "2012-10-17",
+     "Statement": [
+   {
+     "Sid": "VisualEditor0",
+     "Effect": "Allow",
+     "Action": [
+     "kms:Decrypt",
+     "ssm:GetParameterHistory",
+     "ssm:GetParametersByPath",
+     "ssm:GetParameters",
+     "ssm:GetParameter"
+   ],
+     "Resource": [
+     "arn:aws:ssm:us-east-1:871868163784:parameter/dev.frontend.*",
+     "arn:aws:kms:us-east-1:871868163784:key/8ed6ee73-145a-4993-ba10-87a6c6bd693b"
+   ]
+   }
+   ]
+   }
                            ]
-                       })
+   })
 
  }
